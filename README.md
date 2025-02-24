78dca41b8bd69f100de4f7ec8082d453c80b9014
awlxi <sftcrsp@gmail.com>
2025-02-24 21:38:51 +0300

    modifying a gendiff.js and add asciinema in README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/awlxi/fullstack-javascript-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/awlxi/fullstack-javascript-project-46/actions)
+asciinema gendiff.js:
+  https://asciinema.org/a/2MXGDr3Lno2Te65cpP3fc6HKj
--- gendiff.js
+++ gendiff.js
@@ -3,6 +3,23 @@
 const { Command } = require('commander');
 const { parseFile } = require('./parsers');
 const path = require('path');
+
+const genDiff = (data1, data2) => {
+  const allKeys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])].sort();
+  const diff = allKeys.map((key) => {
+    if (!(key in data2)) {
+      return `- ${key}: ${data1[key]}`;
+    } else if (!(key in data1)) {
+      return `+ ${key}: ${data2[key]}`;
+    } else if (data1[key] !== data2[key]) {
+      return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+    } else {
+      return `  ${key}: ${data1[key]}`;
+    }
+  }).join('\n');
+  return diff;
+};
+
 const program = new Command();
 
 program
@@ -16,10 +33,10 @@ program
       const data1 = parseFile(path.resolve(filepath1));
       const data2 = parseFile(path.resolve(filepath2));
 
-      console.log('File 1:', data1);
-      console.log('File 2:', data2);
+      console.log(genDiff(data1, data2));
     } catch (error) {
       console.error('Error:', error.message);
     }
   })
   .parse(process.argv);
+
