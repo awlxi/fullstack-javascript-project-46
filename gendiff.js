d79cce71292ab4bb3721243e1b234af1c24dd9b5
awlxi <sftcrsp@gmail.com>
2025-03-26 14:38:14 +0300

    .

--- gendiff.js
+++ gendiff.js
@@ -6,17 +6,19 @@ const path = require('path');
 
 const genDiff = (data1, data2) => {
   const allKeys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])].sort();
-  const diff = allKeys.map((key) => {
-    if (!(key in data2)) {
-      return `- ${key}: ${data1[key]}`;
-    } else if (!(key in data1)) {
-      return `+ ${key}: ${data2[key]}`;
-    } else if (data1[key] !== data2[key]) {
-      return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
-    } else {
-      return `  ${key}: ${data1[key]}`;
-    }
-  }).join('\n');
+  const diff = allKeys
+    .map((key) => {
+      if (!(key in data2)) {
+        return `- ${key}: ${data1[key]}`;
+      } else if (!(key in data1)) {
+        return `+ ${key}: ${data2[key]}`;
+      } else if (data1[key] !== data2[key]) {
+        return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+      } else {
+        return `  ${key}: ${data1[key]}`;
+      }
+    })
+    .join('\n');
   return diff;
 };
 
@@ -32,11 +34,14 @@ program
     try {
       const data1 = parseFile(path.resolve(filepath1));
       const data2 = parseFile(path.resolve(filepath2));
-
       console.log(genDiff(data1, data2));
     } catch (error) {
       console.error('Error:', error.message);
     }
-  })
-  .parse(process.argv);
+  });
 
+if (require.main === module) {
+  program.parse(process.argv);
+}
+
+module.exports = { genDiff, program };
